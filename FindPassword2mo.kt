5893fc47262140bb13dd7b75109c39d9b9d5fc87
soo00h <cshyeon129@gmail.com>
2023-03-11 19:57:39 +0900

    테스트2

---
+++ FindPassword2mo.kt
@@ -0,0 +1,11 @@
+package com.nycy1020.layouttutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPassword2mo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password2mo)
+    }
+}
